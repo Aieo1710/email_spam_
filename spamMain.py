96e13e9f40105e1f6063f3cb4a2b2be1789e9023
Aieo1710 <140407002+Aieo1710@users.noreply.github.com>
2024-02-21 00:01:29 +0700

    Add files via upload

---
+++ spamMain.py
@@ -0,0 +1,23 @@
+import pickle
+import streamlit as st
+
+
+model=pickle.load(open("spam.pkl", "rb"))
+cv=pickle.load(open("vectorizer.pkl", "rb"))
+
+
+def main():
+    st.title("AI detects :red[spam] messages in emails ✉️")
+    st.subheader(":green[Build With Streamlit & Python]", divider='rainbow')
+    msg=st.text_input("Enter a Text: ")
+    if st.button(":yellow[check]"):
+        data=[msg]
+        vect=cv.transform(data).toarray()
+        prediction=model.predict(vect)
+        result=prediction[0]
+        if result==1:
+            st.error("This is spam")
+        else:
+            st.success("This is not spam")
+
+main()
