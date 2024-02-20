12fb92723c867d81ed411b2c5bf3634cbae85d65
Aieo1710 <140407002+Aieo1710@users.noreply.github.com>
2024-02-21 00:05:14 +0700

    Update spamMain.py

--- spamMain.py
+++ spamMain.py
@@ -2,8 +2,8 @@ import pickle
 import streamlit as st
 
 
-model=pickle.load(open("spam.pkl", "rb"))
-cv=pickle.load(open("vectorizer.pkl", "rb"))
+#model=pickle.load(open("spam.pkl", "rb"))
+#cv=pickle.load(open("vectorizer.pkl", "rb"))
 
 
 def main():
